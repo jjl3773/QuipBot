148be9501fb55605f6d3432b5a930ef6a2526c60
jjl3773 <71155329+jjl3773@users.noreply.github.com>
2020-12-10 09:58:44 -0800

    Create RevealTimer.java

---
+++ src/main/java/timers/RevealTimer.java
@@ -0,0 +1,25 @@
+import net.dv8tion.jda.api.entities.MessageChannel;
+
+import java.util.TimerTask;
+
+public class RevealTimer extends TimerTask {
+    private MessageChannel channel;
+    private String id;
+    private String name;
+
+    public RevealTimer(MessageChannel channel, String id, String name) {
+        this.channel = channel;
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        channel.retrieveMessageById(id).queue((message1) -> {
+            String oldMessage = message1.getContentRaw();
+            message1.editMessage(oldMessage.substring(0, 10) + " by **" + name + "**: "
+                                + oldMessage.substring(12)).queue();
+                }
+        );
+    }
+}
