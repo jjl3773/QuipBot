efb86afb5441812e5aeceeee2a262113096cb814
jjl3773 <71155329+jjl3773@users.noreply.github.com>
2020-12-10 09:37:17 -0800

    Update main.java

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,1 +1,17 @@
+import net.dv8tion.jda.api.JDABuilder;
+import javax.security.auth.login.LoginException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.io.File;
 
+public class main {
+    public static void main(String[] args) throws LoginException, IOException {
+        JDABuilder builder = JDABuilder.createDefault("token");
+        File file = new File("/Users/jason/IdeaProjects/discord/src/main/token.txt");
+        String token = new String(Files.readAllBytes(file.toPath()));
+        builder.setToken(token);
+        GameListener Main = new GameListener(builder);
+        builder.addEventListeners(Main);
+        builder.build();
+    }
+}
