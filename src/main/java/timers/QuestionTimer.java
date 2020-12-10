115350d4dc3e030db5709cc72163c56e4983207a
jjl3773 <71155329+jjl3773@users.noreply.github.com>
2020-12-10 09:57:47 -0800

    Update QuestionTimer.java

--- src/main/java/timers/QuestionTimer.java
+++ src/main/java/timers/QuestionTimer.java
@@ -1,1 +1,15 @@
+import java.util.TimerTask;
 
+public class QuestionTimer extends TimerTask {
+    private Game game;
+
+    public QuestionTimer(Game game) {
+        this.game = game;
+    }
+
+    @Override
+    public void run() {
+        game.setRecordingPrompts(false);
+        game.phase1();
+    }
+}
