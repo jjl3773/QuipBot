df4361fad219cfb185ca5f0463ff6b3965ec6cbd
Jason Liu <71155329+jjl3773@users.noreply.github.com>
2020-12-10 19:31:43 -0800

    Update QuestionTimer.java

--- src/main/java/timers/QuestionTimer.java
+++ src/main/java/timers/QuestionTimer.java
@@ -3,10 +3,13 @@ import java.util.TimerTask;
 public class QuestionTimer extends TimerTask {
     private Game game;
 
+    //Constructor of the "QuestionTimer" class
+    //takes in a Game "game"
     public QuestionTimer(Game game) {
         this.game = game;
     }
 
+    //post: initiates "phase 1" of the game
     @Override
     public void run() {
         game.setRecordingPrompts(false);
