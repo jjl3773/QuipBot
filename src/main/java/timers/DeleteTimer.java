11d149f2516910f65ead34720a96b92994dfba11
Jason Liu <71155329+jjl3773@users.noreply.github.com>
2020-12-10 19:31:23 -0800

    Update DeleteTimer.java

--- src/main/java/timers/DeleteTimer.java
+++ src/main/java/timers/DeleteTimer.java
@@ -4,11 +4,14 @@ public class DeleteTimer extends TimerTask {
     private GameListener Main;
     private Game game;
 
+    //constructor of the "DeleteTimer" class
+    //takes in a GameListener "Main" and a Game "game"
     public DeleteTimer(GameListener Main, Game game) {
         this.Main = Main;
         this.game = game;
     }
 
+    //post: deletes the inputted game
     @Override
     public void run() {
         Main.removeGame(game);
