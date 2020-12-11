0b8367b462c8103daa75e0864b89eb318c5d1af8
Jason Liu <71155329+jjl3773@users.noreply.github.com>
2020-12-10 19:32:02 -0800

    Update RevealTimer.java

--- src/main/java/timers/RevealTimer.java
+++ src/main/java/timers/RevealTimer.java
@@ -7,12 +7,15 @@ public class RevealTimer extends TimerTask {
     private String id;
     private String name;
 
+    //constructor of the "RevealTimer" class
+    //takes in a MessageChannel "channel", String "id", and String "name"
     public RevealTimer(MessageChannel channel, String id, String name) {
         this.channel = channel;
         this.id = id;
         this.name = name;
     }
 
+    //reveals the author of the anonymous prompt
     @Override
     public void run() {
         channel.retrieveMessageById(id).queue((message1) -> {
